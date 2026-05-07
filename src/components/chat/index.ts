bb15e858bac6b82be8ba8cdd83ea1adf2c39cc96
SabiRinl <601098952@qq.com>
2026-05-07 20:12:57 +0800

    del: chatwindow

--- src/components/chat/index.ts
+++ src/components/chat/index.ts
@@ -1,5 +1,3 @@
 export { MessageBubble } from "./MessageBubble";
 export { ChatInput } from "./ChatInput";
 export { QuickActions } from "./QuickActions";
-export { ChatWindow } from "./ChatWindow";
-export { ItineraryPreviewCard } from "./ItineraryPreviewCard";
